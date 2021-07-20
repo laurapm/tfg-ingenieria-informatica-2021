664f9ff6e2400568e7f75add5e7817c95e05390e
laurapm <l.perezm@edu.uah.es>
2021-07-20 21:13:25 +0200

    add redirections in menu side bar

--- frontend/pwa-meteo-clima/src/app/app.module.ts
+++ frontend/pwa-meteo-clima/src/app/app.module.ts
@@ -7,11 +7,26 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Import modules
+import { Platform } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Platform,
+    StatusBar,
+    SplashScreen,
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    }
+  ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
