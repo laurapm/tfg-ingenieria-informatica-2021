582ccde144b2a8c280bf3ff4e0dce626c95f0e19
laurapm <l.perezm@edu.uah.es>
2021-08-02 21:00:34 +0200

    add map to home page

---
+++ backend/meteo-clima-backend/src/main/java/com/tfg/meteoClima/MeteoClimaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.meteoClima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoClimaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoClimaBackendApplication.class, args);
+	}
+
+}
