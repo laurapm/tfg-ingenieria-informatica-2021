fa9e8cc2fe4bf82036ebd83d5719aa2c2761e7be
laurapm <laura.pmedeiro@gmail.com>
2021-08-31 20:25:18 +0200

    add menu side bar

--- frontend/ionic-pwa-meteo-clima/src/app/app.component.ts
+++ frontend/ionic-pwa-meteo-clima/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { Platform } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  activePageTitle = 'Dashboard';
+
+  pages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Contact',
+      url: '/contact',
+      icon: 'mail'
+    }
+  ];
+
+
+  constructor(
+    private platform: Platform,
+    private statusBar: StatusBar,
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+    });
+  }
 }
